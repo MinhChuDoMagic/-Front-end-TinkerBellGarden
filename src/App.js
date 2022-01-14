75380b4237965c69ce9359796aa5af9f2deb7425
minhchubodoiqua <chunhatminh01@gmail.com>
2022-01-14 22:37:36 +0700

    Sua cau truc App.js

--- src/App.js
+++ src/App.js
@@ -13,12 +13,13 @@ import Profile from './components/customer/profile/Profile';
 import stallTicket from './components/employee/nvquay/stallTicket';
 import reception from './components/employee/receptionist/reception';
 import ticketBooking from './components/customer/ticketBooking/ticketBooking';
+import userHome from './components/customer/userHome/Home';
 
 function App() {
   return (<Router>
     <div className="App">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      {/* <Link className="navbar-brand" to={"/"}>positronX.io</Link> */}
+    {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      
       <a className="navbar-brand" href="/">Tinker Bell Garden</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -38,32 +39,36 @@ function App() {
           
           
         </ul>
-        {/* <ul className="navbar-nav mr-auto my-2 my-lg-0" >
-          <li className="nav-item active">
-            <a className="nav-link" href="/">Đăng nhập <span className="sr-only">(current)</span></a>
-          </li>
-        </ul> */}
+        
         <form className="form-inline my-2 my-lg-0">
           <a class="btn btn-outline-success" href="/login" role="button">Đăng nhập</a>
           <p>     </p>
           <a class="btn btn-outline-success" href="/sign-up" role="button">Đăng kí</a>
-          {/* <button className="btn btn-outline-success my-2 my-sm-0 " type="submit" href="/login" >Đăng nhập</button>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Đăng kí</button> */}
+          
         </form>
       </div>
-    </nav>
-          <Switch>
-              <Route exact path='/' component={Home} /> {/*Ô nào làm trang chủ thì thay component trang chủ vào đây */}
-              <Route path="/login" component={Login} />
-              <Route path="/sign-up" component={SignUp} />
-              <Route path="/VIP-register" component={VIPRegister} /> 
-              <Route path="/event1" component={Event} /> 
-              <Route path="/addEvent" component={addEvent} /> 
-              <Route path="/profile" component={Profile} /> 
-              <Route path="/stallTicket" component={stallTicket} />
-              <Route path="/reception" component={reception}/>
-              <Route path="/ticketBooking" component={ticketBooking}/>
-          </Switch>
+    </nav> */}
+          <Route>
+            <Route exact path='/' component={Home} /> {/*Ô nào làm trang chủ thì thay component trang chủ vào đây */}
+            <Route path="/login" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/profile" component={Profile} />
+          </Route>
+
+
+          <Route>
+            <Route path="/userHome" component={userHome} />
+            <Route path="/VIP-register" component={VIPRegister} /> 
+            <Route path="/event1" component={Event} /> 
+            <Route path="/ticketBooking" component={ticketBooking}/>
+          </Route>
+
+          <Route>
+            <Route path="/addEvent" component={addEvent} />               
+            <Route path="/stallTicket" component={stallTicket} />
+            <Route path="/reception" component={reception}/>
+          </Route>
+
     </div></Router>
   );
 }
